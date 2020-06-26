cd6014b2ea782d68278ad3b5ae649bc93595685a
Robert Laverty <roblav96@gmail.com>
2020-06-26 13:47:14 -0400

    prettier --write app

--- app/main-page.ts
+++ app/main-page.ts
@@ -2,29 +2,29 @@ import { Observable, EventData, Page, NavigatedData } from '@nativescript/core'
 import { WebRTC } from 'nativescript-webrtc-plugin'
 
 export function navigatingTo(args: NavigatedData) {
-    let page = args.object as Page
-    page.bindingContext = new MainPage()
+	let page = args.object as Page
+	page.bindingContext = new MainPage()
 }
 
 export function navigatedTo(args: NavigatedData) {
-    // WebRTC.init()
+	// WebRTC.init()
 }
 
 export class MainPage extends Observable {
-    onTap() {
-        let webrtc = new WebRTC({
-            enableAudio: true,
-            enableVideo: true,
-            iceServers: [
-                {
-                    url: 'stun:stun.l.google.com:19302',
-                },
-                {
-                    url: 'serverRequiresAuth',
-                    username: 'username',
-                    password: 'password',
-                },
-            ],
-        })
-    }
+	onTap() {
+		let webrtc = new WebRTC({
+			enableAudio: true,
+			enableVideo: true,
+			iceServers: [
+				{
+					url: 'stun:stun.l.google.com:19302',
+				},
+				{
+					url: 'serverRequiresAuth',
+					username: 'username',
+					password: 'password',
+				},
+			],
+		})
+	}
 }
