4d0d021cfbb58cb4ed2f1b15fb93ce179dcf31c2
Robert Laverty <roblav96@gmail.com>
2020-06-26 13:45:43 -0400

    boilerplate setup

--- app/main-page.ts
+++ app/main-page.ts
@@ -1,30 +1,30 @@
-/*
-In NativeScript, a file with the same name as an XML file is known as
-a code-behind file. The code-behind is a great place to place your view
-logic, and to set up your page’s data binding.
-*/
+import { Observable, EventData, Page, NavigatedData } from '@nativescript/core'
+import { WebRTC } from 'nativescript-webrtc-plugin'
 
-import { EventData, Page } from '@nativescript/core'
-import { HelloWorldModel } from './main-view-model'
+export function navigatingTo(args: NavigatedData) {
+    let page = args.object as Page
+    page.bindingContext = new MainPage()
+}
 
-// Event handler for Page 'navigatingTo' event attached in main-page.xml
-export function navigatingTo(args: EventData) {
-	/*
-    This gets a reference this page’s <Page> UI component. You can
-    view the API reference of the Page to see what’s available at
-    https://docs.nativescript.org/api-reference/classes/_ui_page_.page.html
-    */
-	const page = <Page>args.object
+export function navigatedTo(args: NavigatedData) {
+    // WebRTC.init()
+}
 
-	/*
-    A page’s bindingContext is an object that should be used to perform
-    data binding between XML markup and TypeScript code. Properties
-    on the bindingContext can be accessed using the {{ }} syntax in XML.
-    In this example, the {{ message }} and {{ onTap }} bindings are resolved
-    against the object returned by createViewModel().
-
-    You can learn more about data binding in NativeScript at
-    https://docs.nativescript.org/core-concepts/data-binding.
-    */
-	page.bindingContext = new HelloWorldModel()
+export class MainPage extends Observable {
+    onTap() {
+        let webrtc = new WebRTC({
+            enableAudio: true,
+            enableVideo: true,
+            iceServers: [
+                {
+                    url: 'stun:stun.l.google.com:19302',
+                },
+                {
+                    url: 'serverRequiresAuth',
+                    username: 'username',
+                    password: 'password',
+                },
+            ],
+        })
+    }
 }
