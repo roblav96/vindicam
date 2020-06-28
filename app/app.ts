e6da5733033510b72c45a47c29cead3322a17227
Robert Laverty <roblav96@gmail.com>
2020-06-27 22:10:35 -0400

    [Darwin] M  app/App_Resources/Android/src/main/AndroidManifest.xml

--- app/app.ts
+++ app/app.ts
@@ -1,12 +1,14 @@
-import * as core from '@nativescript/core'
+import { Application, isAndroid } from '@nativescript/core'
 import { WebRTC } from 'nativescript-webrtc-plugin'
 
-core.Application.android.on('activityCreated', function activityCreated(args) {
-	android.os.StrictMode.setThreadPolicy(
-		new android.os.StrictMode.ThreadPolicy.Builder().permitAll().build(),
-	)
-	// WebRTC.init()
-})
+if (isAndroid) {
+	Application.android.on('activityCreated', function activityCreated(args) {
+		android.os.StrictMode.setThreadPolicy(
+			new android.os.StrictMode.ThreadPolicy.Builder().permitAll().build(),
+		)
+		// WebRTC.init()
+	})
+}
 
-core.Application.setCssFileName('app.css')
-core.Application.run('frame')
+Application.setCssFileName('app.css')
+Application.run('frame')
