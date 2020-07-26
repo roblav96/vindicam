0eb932f2434bd3173de7d466dd2ebd30ee52105e
Robert Laverty <roblav96@gmail.com>
2020-07-26 01:14:02 -0400

    trying out rtmp

---
+++ src/views/rtmp.ts
@@ -0,0 +1,17 @@
+import { Observable, EventData, Page, NavigatedData } from '@nativescript/core'
+
+export function navigatingTo(args: NavigatedData) {
+	let page = args.object as Page
+	page.bindingContext = new RtmpPage()
+}
+
+export async function navigatedTo(args: NavigatedData) {
+
+}
+
+export class RtmpPage extends Observable {
+	async tapConnect(args) {
+		console.log(`tapConnect ->`)
+
+	}
+}
